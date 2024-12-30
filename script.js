1814d53fc23018b0f8d0e86a461ee4d5c88b61ac
gelot0208 <139142162+gelot0208@users.noreply.github.com>
2024-12-30 22:32:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// This file can be used for any interactivity you want to add in the future
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portfolio loaded successfully!");
+});
